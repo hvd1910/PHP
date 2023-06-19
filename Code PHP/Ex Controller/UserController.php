29059a235f6a0e98a548df2ea5335523a8aab3a4
Hồ Việt Đức <112045195+shinducql@users.noreply.github.com>
2023-06-19 11:23:40 +0700

    Update UserController.php

--- Code PHP/Ex Controller/UserController.php
+++ Code PHP/Ex Controller/UserController.php
@@ -7,14 +7,8 @@ use http\Env\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class UserController extends BaseController {
-    public function getUser(Request $request) {
-        $user = User::find($request -> user_id);
-
-        return response() -> json($user);
-    }
-    public function updateUser(Request $request)
-    {
-        return 'Thông tin người dùng đã được cập nhật.';
+    public function getAbout( $the_about) {
+        return 'Thông tin này hợp lệ '. $the_about;
     }
 
 }
